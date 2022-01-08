dd74fcd41949a9cd9d35796a00ceadb3822b7064
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-01-08 16:54:41 -0300

    Creation of the position Class

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrez_console.Chessboard;
 
 namespace xadrez_console
 {
@@ -6,7 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position position = new Position(3, 4);
+
+            Console.WriteLine("Position: " + position);
         }
     }
 }
