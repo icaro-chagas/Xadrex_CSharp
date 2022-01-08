e18623aea750fba0b9845425b32b66b3f08be8c7
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-01-08 19:54:24 -0300

    Improvement of the PrintScreen method

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,12 +8,24 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            ChessPosition cPos = new ChessPosition('c', 7);
+            try
+            {
+                Board board = new Board(8, 8);
 
+                board.PlacePiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.PlacePiece(new Rook(board, Color.Black), new Position(1, 3));
+                board.PlacePiece(new King(board, Color.Black), new Position(0, 2));
 
-            Console.WriteLine(cPos);
+                board.PlacePiece(new Rook(board, Color.White), new Position(3, 5));
+                board.PlacePiece(new Rook(board, Color.White), new Position(7, 7));
+                board.PlacePiece(new King(board, Color.White), new Position(7, 2));
 
-            Console.WriteLine(cPos.ToPositon());
+                Screen.PrintSreen(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
